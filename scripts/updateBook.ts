112148f62fc73dcdf801f03ec83a1e7a4c50f08c
praniil <pparajuli15@gmail.com>
2024-07-19 21:02:21 +0545

    feat: added update book script

---
+++ scripts/updateBook.ts
@@ -0,0 +1,21 @@
+import hre from "hardhat"
+
+const address = "0x83566E1037b996cB8947AACa4D4A7a2d86D9430B"
+
+async function main() {
+    const [owner, otherAccount] = await hre.ethers.getSigners()
+
+    console.log(owner.address)
+    console.log(otherAccount.address)
+
+    const crud = await hre.ethers.getContractAt("CRUD", address)
+    const updateBook = await crud.updateBook(1, "University Chemistry", "Rollie J. Myers")
+    await updateBook.wait();
+    const readBook = await crud.readBook(1)
+    console.log(readBook)
+}
+
+main().catch((error) => {
+    console.log(error)
+    process.exitCode = 1;
+})
